319957a988a72745949cc6840b9bd136f8f3091c
Asive Dlaba <adlaba@csir.co.za>
2016-10-01 01:32:50 +0200

    feat(api): create views and models

--- waterly/api/models.py
+++ waterly/api/models.py
@@ -2,4 +2,28 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    building_name = models.CharField(max_length=30)
+    street_no = models.IntegerField()
+    suburb_name = models.CharField(max_length=30)
+    municipality_name = models.CharField(max_length=30)
+    province_name = models.CharField(max_length=30)
+
+
+class Meter(models.Model):
+    meter_no = models.CharField(max_length=15, primary_key=True)
+    daily_consumption = models.FloatField()
+
+
+class Consumer(models.Model):
+    name = models.CharField(max_length=30)
+    meter = models.OneToOneField(
+            Meter,
+            on_delete=models.CASCADE,
+            primary_key=True,
+    )
+    address = models.OneToOneField(
+            Address,
+            on_delete=models.CASCADE,
+    )
--- waterly/api/views.py
+++ waterly/api/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.test import APIClient
 
-# Create your views here.
+from api.models  import Consumer
+
+
+class GetConsumerData(generics.ListCreateAPIView):
+    """Get data for particular consumer in the city.
+    
+    """
+
+    def get(self, *args, **kwargs):
+        """Given identifier for the consumer get their current data
+        
+        Kwargs:
+            kwargs['pk'] (str): Meter number for the consumer.
+
+        """
+
+        pk = kwargs['pk']
+        queryset = self.get_queryset(pk)
+        serializer = ConsumerSerializer(queryset, many=False)
+
+        return Response(serializer.data)
+
+
+class GetCreateDatasets(generics.ListCreateAPIView):
+    """Get or create consumer entry on the api.
+    
+    Attributes:
+        queryset (models): All consumers.
+        serializer_class (object): Serializer (json)
+            
+    """
+
+    queryset = Consumer.objects.all()
+    serializer_class = ConsumerSerializer
+
