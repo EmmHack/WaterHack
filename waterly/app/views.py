f368186728d7d9303fe27e2dee236cf3099c4638
Ofentswe1 <ofentswel@gmail.com>
2016-09-30 22:24:36 -0700

    forms and post method

--- waterly/app/views.py
+++ waterly/app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
-
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 class Home(View):
@@ -14,7 +16,32 @@ class Login(View):
     template_name = 'app/login.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+
+        errors = ""
+        next = ""
+        form = ""
+        if self.request.method == 'GET':
+            return render(self.request, 'app/login.html', {'form': form})
+
+        elif request.method == 'POST':
+
+            try:
+                username = self.request.POST.get('username')
+                password = self.request.POST.get('password')
+                user = auth.authenticate(username=username, password=password)
+                if user is not None and user.is_active:
+                    auth.login(request, user)
+                    request.session['username'] = request.POST.get('username')
+                    return HttpResponseRedirect('/app/')
+                else:
+                    errors = "Invalid username or password"
+
+            except User.DoesNotExist:
+                errors = "Invalid username or password"
+                print "none"
+            return render(self.request,'app/login.html', {'form': form, 'errors': errors})
+
+        return render(self.request, self.template_name)
 
 
 class SignUp(View):
