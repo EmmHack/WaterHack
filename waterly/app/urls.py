2930e13b020f5cc99b5763ea86c43110f22e1f7f
Ofentswe1 <ofentswel@gmail.com>
2016-10-01 11:56:16 -0700

    hello

--- waterly/app/urls.py
+++ waterly/app/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from views import Home, SignUp, Login
+from views import Home, login,register
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^$', Home.as_view(), name='home'),
     url(r'^home', Home.as_view(), name='home'),
-    url(r'^signup', SignUp.as_view(), name='signup'),
-    url(r'^login', Login.as_view(), name='login'),
+    url(r'^signup/{0,1}$', register, name='signup'),
+    url(r'^(?:accounts/){0,1}log((?:in)|(?:out))/{0,1}$', login,name='loginout'),
 ]
--- waterly/app/views.py
+++ waterly/app/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from django.views import View
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from models import register_form
 
+def login(request, in_or_out):
+    out = True if "out" in in_or_out else False
+    ignore = (request.POST.get('todo') == 'redirect')
+    if ignore:
+        out = False
+    reg = request.GET.get('reg', False)
+    next = request.GET.get(u'next', '/app/home/')
+    valid = True
+    if request.POST and not out and not ignore:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(next)
+        else:
+            valid = False
+    if "out" in in_or_out and not ignore:
+            auth.logout(request)
+            request.session['username'] = request.POST.get('username')
+    return render(request,'app/login.html', {'in_or_out': out, 'reg': reg, 'valid': valid, 'next': next})
+    #return render(request,'app/login.html')
 
 # Create your views here.
 class Home(View):
@@ -12,55 +35,22 @@ class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
+def register(request):
+    """
 
-class Login(View):
-    template_name = 'app/login.html'
-
-    def get(self, request, *args, **kwargs):
-
-        errors = ""
-        next = ""
-        if self.request.method == 'GET':
-            return render(self.request, self.template_name)
-
-        elif request.method == 'POST':
-
-            try:
-                username = self.request.POST.get('username')
-                password = self.request.POST.get('password')
-                user = auth.authenticate(username=username, password=password)
-                if user is not None and user.is_active:
-                    auth.login(self.request, user)
-                    request.session['username'] = self.request.POST.get('username')
-                    return HttpResponseRedirect('/app/')
-                else:
-                    errors = "Invalid username or password"
-
-            except User.DoesNotExist:
-                errors = "Invalid username or password"
-                print "none"
-            return render(self.request, self.template_name, {'errors': errors})
-
-
-class SignUp(View):
-    template_name = 'app/signup.html'
-
-    def get(self, request, *args, **kwargs):
-
-        if self.request.method == 'GET':
-            return render(self.request, self.template_name)
-        elif self.request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-
-            if User.objects.filter(username=username).exists():
-                errors = "Username already Taken"
-                return render(self.request, self.template_name, {"errors": errors})
-
-            elif User.objects.filter(username=username).exists():
-                errors = "Email already Registered"
-                return render(self.request, self.template_name, {"errors": errors})
-            else:
-                client = User.objects.create_user(username=username, password=password)
-                client.save()
-                return render(self.request, self.template_name)
+    :param request:
+    :return:
+    """
+    duplicate = False
+    if request.POST:
+        username = request.POST.get('username')
+        if User.objects.filter(username=username):
+            duplicate = True
+        else:
+            user = User.objects.create_user(username=username, password=request.POST.get('password'))
+            user.username = request.POST.get('username')
+            user.password = request.POST.get('password'),
+            user.save()
+            return HttpResponseRedirect("/app/login/?reg=1")
+    form = register_form()
+    return render(request,'app/signup.html', {'form': form, 'duplicate': duplicate})
